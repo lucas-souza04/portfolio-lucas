b553f9e8d28ee5e4ce7f71fad0406c3edc034a84
Lucas Souza <168837793+lucas-souza04@users.noreply.github.com>
2024-05-07 23:29:24 -0300

    Inserindo arquivos do meu portfolio

---
+++ email.php
@@ -0,0 +1,19 @@
+<?php
+
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email']);
+$nome = addslashes($_POST['telefone']);
+$mensagem = addslashes($_POST['mensagem']);
+
+$destino = "newcontato.portfolio@gmail.com";
+$assunto = "Coleta de dados - Portfolio";
+
+$corpo = "Nome: " . $nome . "\n" . "Email: " . $email . "\n" . "Telefone: " . $telefone . "\n" . $mensagem;
+
+$cabecalho = "From newcontato.portfolio@gmail.com" . "\n" . "Reply-to: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
+
+if (mail($destino, $assunto, $corpo, $cabecalho)) {
+  echo ("E-mail enviado com sucesso!");
+} else {
+  echo ("Houve um erro ao tentar enviar o e-mail!");
+}
